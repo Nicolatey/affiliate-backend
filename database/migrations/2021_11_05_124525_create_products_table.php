49413a25e64d8a88eda81a21743df29cbd47fbbd
nsluis9446 <nsluis9446@student.landstede.nl>
2021-11-05 16:00:58 +0100

    added product tables

--- database/migrations/2021_11_05_124525_create_products_table.php
+++ database/migrations/2021_11_05_124525_create_products_table.php
@@ -15,6 +15,11 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->double('price');
+            $table->string('url');
+            $table->string('image');
             $table->timestamps();
         });
     }
